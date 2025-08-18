f91460017befca0d9df3199d737b1a7320010c06
Steven Linde <stevenlinde@google.com>
2025-08-18 03:19:31 +0000

    ignoring coverage file/html and removing blocking test

--- cmd/proxy_test.go
+++ cmd/proxy_test.go
@@ -1,13 +1,9 @@
 package cmd
 
-import (
-	"testing"
-)
-
 // This unit test executes the proxy command with default flags so we can debug
-func TestProxyCommand(t *testing.T) {
-	rootCmd.SetArgs([]string{"proxy"})
+// func TestProxyCommand(t *testing.T) {
+// 	rootCmd.SetArgs([]string{"proxy"})
 
-	rootCmd.Execute()
+// 	rootCmd.Execute()
 
-}
+// }
